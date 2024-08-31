4e43112dc51d6976b4f1fc80c4988d4534584d4a
J0sueTM <teodoro.josue@protonmail.ch>
2024-08-30 21:35:49 -0300

    style: mv router data into lets

--- src/j0suetm/teia/router.clj
+++ src/j0suetm/teia/router.clj
@@ -13,25 +13,31 @@
 (defn build
   [routes & options]
   (let [{:keys [default-format]
-         :or {default-format "application/edn"}} options]
+         :or {default-format "application/edn"}} options
+
+        coercion (reitit.coercion.malli/create
+                  {:error-keys #{:type :in :value :errors
+                                 :humanized :schema}
+                   :compile malli.util/closed-schema
+                   :strip-extra-keys true
+                   :default-values true
+                   :options nil})
+
+        muuntaja (muuntaja/create
+                  (assoc
+                   muuntaja/default-options
+                   :default-format default-format))
+
+        middlewares [reitit.mddwr.ex/exception-middleware
+                     reitit.mddwr.muuntaja/format-negotiate-middleware
+                     reitit.mddwr.muuntaja/format-response-middleware
+                     reitit.mddwr.muuntaja/format-request-middleware
+                     reitit.coercion/coerce-response-middleware
+                     reitit.coercion/coerce-request-middleware
+                     reitit.mddwr.params/parameters-middleware]]
     (reitit/router
      routes
      {:exception reitit.pretty/exception
-      :data {:coercion (reitit.coercion.malli/create
-                        {:error-keys #{:type :in :value :errors
-                                       :humanized :schema}
-                         :compile malli.util/closed-schema
-                         :strip-extra-keys true
-                         :default-values true
-                         :options nil})
-             :muuntaja (muuntaja/create
-                        (assoc
-                         muuntaja/default-options
-                         :default-format default-format))
-             :middleware [reitit.mddwr.ex/exception-middleware
-                          reitit.mddwr.muuntaja/format-negotiate-middleware
-                          reitit.mddwr.muuntaja/format-response-middleware
-                          reitit.mddwr.muuntaja/format-request-middleware
-                          reitit.coercion/coerce-response-middleware
-                          reitit.coercion/coerce-request-middleware
-                          reitit.mddwr.params/parameters-middleware]}})))
+      :data {:coercion coercion
+             :muuntaja muuntaja
+             :middleware middlewares}})))
