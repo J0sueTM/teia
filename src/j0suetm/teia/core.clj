b9c60af36902534c16f58775e78ed6dd025948d5
J0sueTM <teodoro.josue@protonmail.ch>
2024-08-31 16:44:37 -0300

    doc: configure marginalia

--- src/j0suetm/teia/core.clj
+++ src/j0suetm/teia/core.clj
@@ -1,7 +1,3 @@
 (ns j0suetm.teia.core
-  "This Source Code Form is subject to the terms of the Mozilla Public
-  License, v. 2.0. If a copy of the MPL was not distributed with this
-  file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-  Teia is a Simple, Fast and Reliable HyperMedia library built on top
+  "Teia is a Simple, Fast and Reliable HyperMedia library built on top
   of HTMX, leveraging its Power and Simplicity.")
