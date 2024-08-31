1bb89431982c012c0db978a54f82b4e5b3da41b8
J0sueTM <teodoro.josue@protonmail.ch>
2024-08-31 16:25:44 -0300

    doc: philosophy

--- src/j0suetm/teia/core.clj
+++ src/j0suetm/teia/core.clj
@@ -1,1 +1,7 @@
-(ns j0suetm.teia.core)
+(ns j0suetm.teia.core
+  "This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+  Teia is a Simple, Fast and Reliable HyperMedia library built on top
+  of HTMX, leveraging its Power and Simplicity.")
