cb16c0be9bdae217c57259ef5ae11ea306cf0b5f
J0sueTM <teodoro.josue@protonmail.ch>
2024-08-31 18:04:27 -0300

    feat: `text/html` muuntaja format

--- src/j0suetm/teia/router.clj
+++ src/j0suetm/teia/router.clj
@@ -1,19 +1,50 @@
 (ns j0suetm.teia.router
   (:require
+   [hiccup2.core :as hiccup]
    [malli.util]
    [muuntaja.core :as muuntaja]
+   [muuntaja.format.core :as muuntaja.fmt]
    [reitit.coercion.malli :as reitit.coercion.malli]
    [reitit.dev.pretty :as reitit.pretty]
    [reitit.ring :as reitit]
    [reitit.ring.coercion :as reitit.coercion]
    [reitit.ring.middleware.exception :as reitit.mddwr.ex]
    [reitit.ring.middleware.muuntaja :as reitit.mddwr.muuntaja]
-   [reitit.ring.middleware.parameters :as reitit.mddwr.params]))
+   [reitit.ring.middleware.parameters :as reitit.mddwr.params])
+  (:import
+   [java.io OutputStream]))
+
+(def html-format
+  "Muuntaja format to for text/html"
+  (let [enc-fn (fn [_]
+                 (reify
+                   ;;
+                   muuntaja.fmt/EncodeToBytes
+                   (encode-to-bytes [_ data charset]
+                     (.getBytes
+                      (str (hiccup/html data))
+                      ^String charset))
+                   ;;
+                   muuntaja.fmt/EncodeToOutputStream
+                   (encode-to-output-stream [_ data charset]
+                     (fn [^OutputStream output-stream]
+                       (.write
+                        output-stream
+                        (.getBytes
+                         (str (hiccup/html data))
+                         ^String charset))))))]
+    (muuntaja.fmt/map->Format
+     {:name "text/html"
+      :encoder [enc-fn]})))
 
 (defn build
-  [routes & options]
+  "Builds a basic ring router."
+  [routes & [options]]
   (let [{:keys [default-format]
-         :or {default-format "application/edn"}} options
+         ;; assumes UI router by default
+         :or {default-format "text/html"}} options
+
+        ui-router? (= default-format "text/html")
 
         coercion (reitit.coercion.malli/create
                   {:error-keys #{:type :in :value :errors
@@ -24,17 +55,22 @@
                    :options nil})
 
         muuntaja (muuntaja/create
-                  (assoc
-                   muuntaja/default-options
-                   :default-format default-format))
+                  (-> (assoc-in
+                       muuntaja/default-options
+                       [:formats "text/html"]
+                       html-format)
+                      (assoc :default-format default-format)))
 
-        middlewares [reitit.mddwr.ex/exception-middleware
-                     reitit.mddwr.muuntaja/format-negotiate-middleware
-                     reitit.mddwr.muuntaja/format-response-middleware
-                     reitit.mddwr.muuntaja/format-request-middleware
-                     reitit.coercion/coerce-response-middleware
-                     reitit.coercion/coerce-request-middleware
-                     reitit.mddwr.params/parameters-middleware]]
+        ;; doesn't need to verify response when UI router
+        middlewares (into
+                     [reitit.mddwr.ex/exception-middleware
+                      reitit.mddwr.muuntaja/format-negotiate-middleware
+                      reitit.mddwr.muuntaja/format-request-middleware
+                      reitit.mddwr.muuntaja/format-response-middleware
+                      reitit.coercion/coerce-request-middleware
+                      reitit.mddwr.params/parameters-middleware]
+                     (when ui-router?
+                       [reitit.coercion/coerce-response-middleware]))]
     (reitit/router
      routes
      {:exception reitit.pretty/exception
--- test/j0suetm/teia/router_test.clj
+++ test/j0suetm/teia/router_test.clj
@@ -10,21 +10,38 @@
                  {:get
                   {:parameters {:path {:name :string}}
                    :responses {200 [:map [:username :string]]}
-                   :handler (fn [{:keys [headers path-params]}]
+                   :handler (fn [{:keys [path-params]}]
                               (let [{:keys [name]} path-params]
                                 {:status 200
-                                 :headers (when-let [accept (:accept headers)]
-                                            {:content-type accept})
-                                 :body {:username name}}))}}]]]
+                                 :body {:username name}}))}}]
+                ["/users/:name/ui"
+                 {:get
+                  {:parameters {:path {:name :string}}
+                   :handler (fn [{:keys [path-params]}]
+                              (let [{:keys [name]} path-params]
+                                {:status 200
+                                 :headers {:content-type "text/html"}
+                                 :body [:p "hello " name]}))}}]]]
     (t/testing "supported formats;"
-      (t/testing "default format;"
+      (t/testing "html;"
+        (t/is (= "<p>hello j0suetm</p>"
+                 (->> ((reitit/ring-handler
+                        (teia.router/build routes))
+                       {:request-method :get
+                        :uri "/users/j0suetm/ui"})
+                      (:body)
+                      (slurp)))))
+      (t/testing "edn;"
         (t/is (= {:username "j0suetm"}
-                 (->> ((reitit/ring-handler (teia.router/build routes))
+                 (->> ((reitit/ring-handler
+                        (teia.router/build
+                         routes
+                         {:default-format "application/edn"}))
                        {:request-method :get
                         :uri "/users/j0suetm"})
                       (:body)
                       (muuntaja/decode "application/edn")))))
-      (t/testing "others (json only, for simplicity);"
+      (t/testing "json;"
         (t/is (= {:username "j0suetm"}
                  (->> ((reitit/ring-handler
                         (teia.router/build
@@ -33,5 +50,4 @@
                        {:request-method :get
                         :uri "/users/j0suetm"})
                       (:body)
-                      (muuntaja/decode "application/json"))))))
-    (t/testing "html format;")))
+                      (muuntaja/decode "application/json"))))))))
